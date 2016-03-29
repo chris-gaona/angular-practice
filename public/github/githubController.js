68b5461fa4532e7255ff90af330f0c7251d00f52
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-28 22:02:20 -0700

    Successfully completes Angular routes tutorial; uses  get to my github api

--- public/github/githubController.js
+++ public/github/githubController.js
@@ -1,7 +1,14 @@
 angular.module('routesApp')
 
-.controller('githubCtrl', ['$scope', '$state', function($scope, $state) {
+.controller('githubCtrl', ['$scope', '$state', '$http', function($scope, $state, $http) {
   $scope.changeState = function(stateName) {
     $state.go(stateName);
   };
+
+  $http.get('https://api.github.com/users/chris-gaona').success(function(data) {
+    $scope.user = data;
+    console.log(data);
+  }).error(function() {
+    console.log('error in get request');
+  });
 }]);
